923bda6a91fedd193547668cc68a461c054e7790
Hessam Imen <hessam.imen@gmail.com>
2022-06-25 14:29:58 +1000

    Done with assignmnet

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        anagrams.map do |element|
+        element if element.split("").sort == @word.split("").sort
+        end.compact
+    end
+end
